b0a8af09a9ea3f9bd64a5a6cceed69f1db817a93
NoviAmeiliaPutri <noviemilia7@gmail.com>
2022-04-18 12:20:34 +0700

    Initial commit

---
+++ app/src/main/java/com/sayarozi/recyclerviewkotlin/Superhero.kt
@@ -0,0 +1,13 @@
+package com.sayarozi.recyclerviewkotlin
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+
+data class Superhero(
+    val imgSuperhero: Int,
+    val nameSuperhero: String,
+    val descSuperhero: String,
+
+)   : Parcelable
